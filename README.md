a876e55a651b1ecf9d9f0f62dbae73cfdca5c6ca
Stefan Furstenberg <stefan.furstenberg@twint.ch>
2025-04-03 11:00:13 +0200

    Adding tf files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # llm
-Infrastructure for LLM
+
+Infrastructure for Openweb UI and Lite LLM running on Hetzner
+
+## Steps:
+1. Link to Terraform Cloud Workspace.
+1. Sign into TFC, create project and workspace (API-driven workflow)
+1. The TFC free plan does not support teams :(
+
---
+++ terraform/hetzner/nue/main.tf
@@ -0,0 +1,17 @@
+terraform { 
+  cloud { 
+    
+    organization = "Furstenberg-Consulting" 
+
+    workspaces { 
+      name = "github-actions" 
+    } 
+  } 
+}
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
