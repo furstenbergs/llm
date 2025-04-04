f5a802b828645c0c740348e3c76dce3395078b59
Stefan Furstenberg <stefan@furstenbergs.com>
2025-04-04 15:35:05 +0200

    update vars

--- terraform/modules/hcloud_network/variables.tf
+++ terraform/modules/hcloud_network/variables.tf
@@ -1,31 +1,16 @@
 # modules/hcloud_network/variables.tf
 
-variable "network_name" {
-  description = "The name of the Hetzner network."
+variable "llm_private_network" {
+  description = "The IP range for the private network."
   type        = string
 }
 
-variable "network_ip_range" {
-  description = "The IP range for the Hetzner network."
+variable "llm_private_subnet" {
+  description = "The IP range for the private subnet."
   type        = string
 }
 
 variable "network_zone" {
   description = "The network zone for the subnet."
   type        = string
-}
-
-variable "subnet_ip_range" {
-  description = "The IP range for the Hetzner subnet."
-  type        = string
-}
-
-variable "firewall_name" {
-  description = "The name of the Hetzner firewall."
-  type        = string
-}
-
-variable "allowed_ips" {
-  description = "The list of allowed IPs for the firewall rule."
-  type        = list(string)
 }
